d1f9ea97f31d115c964f400ab19bb73e6dca9172
xxria17 <xxrcy32@gmail.com>
2021-04-23 16:22:22 +0900

    5번

--- src/test/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ src/test/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -1,10 +1,15 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao getUserDao() {
         return new UserDao(getConnection());
     }
-
+    @Bean
     public ConnectionMaker getConnection() {
         return new JejuConnectionMaker();
     }
--- src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -1,7 +1,10 @@
 package kr.ac.jejunu.userdao;
 
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -9,14 +12,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
-    DaoFactory daoFactory = new DaoFactory();
+    private static UserDao userDao;
+
+    String name = "hulk";
+    String password = "1234";
+
+    @BeforeAll
+    public static void setup() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        userDao = applicationContext.getBean("getUserDao", UserDao.class);
+    }
 
     @Test
     public void testGetJeju() throws SQLException, ClassNotFoundException {
         Integer id = 1;
-        String name = "hulk";
-        String password = "1234";
-        UserDao userDao = daoFactory.getUserDao();
         User user = userDao.get(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -25,10 +34,6 @@ public class UserDaoTests {
 
     @Test
     public void testInsertJeju() throws SQLException, ClassNotFoundException {
-        String name = "hulk";
-        String password = "1234";
-
-        UserDao userDao = daoFactory.getUserDao();
         Integer id = userDao.insert(name, password);
 
         User user = userDao.get(id);
