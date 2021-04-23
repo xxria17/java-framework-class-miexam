4122ccf845d0ed10c7c2e24255612ac8fcbb9f68
xxria17 <xxrcy32@gmail.com>
2021-04-23 16:28:34 +0900

    6번

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -1,14 +1,15 @@
 package kr.ac.jejunu.userdao;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    ConnectionMaker connectionMaker;
-    UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    DataSource dataSource;
+    UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
-    public User get(Integer id) throws ClassNotFoundException, SQLException {
-        Connection connection = connectionMaker.getConnection();
+    public User get(Integer id) throws SQLException {
+        Connection connection = dataSource.getConnection();
         PreparedStatement preparedStatement =
                 connection.prepareStatement("select * from userinfo where id = ?");
         preparedStatement.setLong(1, id);
@@ -28,8 +29,8 @@ public class UserDao {
         return user;
     }
 
-    public Integer insert(String name, String password) throws SQLException, ClassNotFoundException {
-        Connection connection = connectionMaker.getConnection();
+    public Integer insert(String name, String password) throws SQLException {
+        Connection connection = dataSource.getConnection();
         PreparedStatement preparedStatement =
                 connection.prepareStatement("insert into userinfo (name,password) values (?,?)",
                         Statement.RETURN_GENERATED_KEYS);
--- src/test/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ src/test/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -1,16 +1,39 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 public class DaoFactory {
+    @Value("${db.classname}")
+    private String classname;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
+
     @Bean
     public UserDao getUserDao() {
-        return new UserDao(getConnection());
+        return new UserDao(dataSource());
     }
     @Bean
-    public ConnectionMaker getConnection() {
-        return new JejuConnectionMaker();
+    public DataSource dataSource() {
+        SimpleDriverDataSource simpleDriverDataSource = new SimpleDriverDataSource();
+        try {
+            simpleDriverDataSource.setDriverClass((Class<? extends Driver>) Class.forName(classname));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        simpleDriverDataSource.setUrl(url);
+        simpleDriverDataSource.setUsername(username);
+        simpleDriverDataSource.setPassword(password);
+        return simpleDriverDataSource;
     }
 }
